79cdfd8026bba58276afcad02162e8104a502c74
Polina Matavina <paulin.mipt@gmail.com>
2015-10-05 05:54:27 +0300

    #12 silly toolbar with pictograms

---
+++ Race/toolbar.h
@@ -0,0 +1,32 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define cmdTabFile 30000 
+#define cmdTabFile_LabelTitle_RESID 60001
+#define cmdGroupMain 30001 
+#define cmdGroupMain_LabelTitle_RESID 60002
+#define cmdSaveMap 30002 
+#define cmdSaveMap_LabelTitle_RESID 60003
+#define cmdSaveMap_TooltipTitle_RESID 60004
+#define cmdSaveMap_TooltipDescription_RESID 60005
+#define cmdSaveMap_LargeImages_RESID 60006
+#define cmdLoadMap 30003 
+#define cmdLoadMap_LabelTitle_RESID 60007
+#define cmdLoadMap_TooltipTitle_RESID 60008
+#define cmdLoadMap_TooltipDescription_RESID 60009
+#define cmdLoadMap_LargeImages_RESID 60010
+#define cmdNewMap 30004 
+#define cmdNewMap_LabelTitle_RESID 60011
+#define cmdNewMap_TooltipTitle_RESID 60012
+#define cmdNewMap_TooltipDescription_RESID 60013
+#define cmdNewMap_LargeImages_RESID 60014
+#define cmdExit 30010 
+#define cmdExit_LabelTitle_RESID 60015
+#define cmdExit_TooltipTitle_RESID 60016
+#define cmdExit_TooltipDescription_RESID 60017
+#define cmdExit_LargeImages_RESID 60018
+#define InternalCmd2_LabelTitle_RESID 60019
