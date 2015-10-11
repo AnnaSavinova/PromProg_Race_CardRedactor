090840a0a39e53b04e4e5ba315176f09ffeea6ef
unknown <paulin.mipt@gmail.com>
2015-10-11 03:09:14 +0300

    #4 init state

--- Race/CMap.cpp
+++ Race/CMap.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "CMap.h"
 
-CMap::CMap() : sizeX( 8 ), sizeY( 6 ), colorsNumber( 3 )
+CMap::CMap() : sizeX( 20 ), sizeY( 15 ), colorsNumber( 3 )
 {}
 
 CMap::~CMap()
@@ -54,13 +54,72 @@ void CMap::SaveMapToFile( std::ofstream & fout )
 
 void CMap::RestartMap()
 {
-    numbers = std::vector< std::vector<int> >( sizeY );
-    for( int i = 0; i < sizeY; i++ ) {
-        numbers[i].resize( sizeX );
-        for( int j = 0; j < sizeX; j++ ) {
-            numbers[i][j] = 0;
-        }
-    }
+	numbers = std::vector< std::vector<int> >( sizeY );
+
+	int border = 1; // forest border
+	int roadWidth = 3;
+
+	if ( sizeX <= 10 || sizeY <= 10 ) {
+		border = 0;
+		roadWidth = 2;
+	}
+
+	const int ROAD = 1;
+	const int FOREST = 0;
+	const int WALL = 2;
+
+	for ( int i = 0; i < sizeY; i++ ) {
+		numbers[i].resize( sizeX ); // now forest is everywhere
+
+		if ( i == border || i == sizeY - 1 - border ) { // FOREST-WALL-FOREST
+			for ( int j = border; j < sizeX - border; j++ ) {
+				numbers[i][j] = WALL;
+			}
+		}
+		else if ( ( i > border && i < border + roadWidth + 1 ) ||
+			( i > sizeY - 2 - border - roadWidth && i < sizeY - 1 - border ) ) // FOREST-WALL-ROAD-WALL-FOREST
+		{
+			// set walls
+			numbers[i][border] = numbers[i][sizeX - 1 - border] = WALL;
+
+			// set road
+			for ( int j = border + 1; j < sizeX - 1 - border; j++ ) {
+				numbers[i][j] = ROAD;
+			}
+
+		}
+		else if ( i == border + roadWidth + 1 || i == sizeY - 2 - border - roadWidth ) { // FOREST-WALL-ROAD-WALL-ROAD-WALL-FOREST
+																						 // set walls
+			numbers[i][border] = numbers[i][sizeX - 1 - border] = WALL;
+
+			for ( int j = border + roadWidth + 1; j < sizeX - 1 - border - roadWidth; j++ ) {
+				numbers[i][j] = WALL;
+			}
+
+			// set road
+			for ( int j = border + 1; j < border + roadWidth + 1; j++ ) {
+				numbers[i][j] = ROAD;
+			}
+
+			for ( int j = sizeX - 1 - border - roadWidth; j < sizeX - 1 - border; j++ ) {
+				numbers[i][j] = ROAD;
+			}
+		}
+		else if ( i > border + roadWidth + 1 && i < sizeY - 2 - border - roadWidth ) { // FOREST-WALL-ROAD-WALL-FOREST-WALL-ROAD-WALL
+			// set walls
+			numbers[i][border] = numbers[i][sizeX - 1 - border] = WALL;
+			numbers[i][border + roadWidth + 1] = numbers[i][sizeX - 2 - border - roadWidth] = WALL;
+
+			// set road
+			for ( int j = border + 1; j < border + roadWidth + 1; j++ ) {
+				numbers[i][j] = ROAD;
+			}
+
+			for ( int j = sizeX - 1 - border - roadWidth; j < sizeX - 1 - border; j++ ) {
+				numbers[i][j] = ROAD;
+			}
+		}
+	}
 }
 
 void CMap::SetSize( int x, int y )
